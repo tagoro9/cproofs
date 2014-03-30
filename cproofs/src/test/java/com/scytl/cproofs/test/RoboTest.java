762a6f89464572c8259418703026f927d70021bd
Victor <tagoro9@gmail.com>
2014-03-30 15:15:20 +0100

    chore #68500262 - configure robolectric tests execution

---
+++ cproofs/src/test/java/com/scytl/cproofs/test/RoboTest.java
@@ -0,0 +1,14 @@
+package com.scytl.cproofs.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class RoboTest {
+    @Test
+    public void testTrueIsTrue() throws Exception {
+        assertEquals(true, true);
+    }
+}
